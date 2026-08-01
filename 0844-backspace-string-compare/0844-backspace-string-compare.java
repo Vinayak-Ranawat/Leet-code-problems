f326b10fd12083bb618a680f5dcf5a3bf4f66beb
Vinayak-Ranawat <vinayak.ranawat1501@gmail.com>
2026-08-01 21:01:58 +0530

    Time: 0 ms (100%), Space: 42.9 MB (79.44%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) {
+            while (i >= 0) { // find real char in s
+                if (s.charAt(i) == '#') {
+                    skipS++; i--;
+                } else if (skipS > 0) {
+                    skipS--; i--;
+                } else break;
+            }
+
+            while (j >= 0) { // find real char in t
+                if (t.charAt(j) == '#') {
+                    skipT++; j--;
+                } else if (skipT > 0) {
+                    skipT--; j--;
+                } else break;
+            }
+
+            // compare both strings
+            if (i >= 0 && j >= 0) {
+                if (s.charAt(i) != t.charAt(j))
+                    return false;
+            } else if (i >= 0 || j >= 0) {
+                return false;
+            }
+            i--;
+            j--;
+        }
+        return true;
+    }
+}
