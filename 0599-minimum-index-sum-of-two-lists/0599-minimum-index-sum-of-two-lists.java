55b7d22b5cbe8f7ead8894013d39e8a3b1122805
Vinayak-Ranawat <vinayak.ranawat1501@gmail.com>
2026-09-18 20:53:37 +0530

    Time: 8 ms (97.58%), Space: 47.6 MB (67.47%) - LeetHub

---
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+       HashMap<String, Integer> map = new HashMap<>();
+        List<String> result = new ArrayList<>();
+        int minSum = Integer.MAX_VALUE;
+
+        // store list1 with index
+        for (int i = 0; i < list1.length; i++) {
+            map.put(list1[i], i);
+        }
+
+        // scan list2
+        for (int j = 0; j < list2.length; j++) {
+            if (map.containsKey(list2[j])) {
+                int sum = map.get(list2[j]) + j;
+                if (sum < minSum) {
+                    result.clear();
+                    result.add(list2[j]);
+                    minSum = sum;
+                } else if (sum == minSum) {
+                    result.add(list2[j]);
+                }
+            }
+        }
+        return result.toArray(new String[0]);
+    }
+}
